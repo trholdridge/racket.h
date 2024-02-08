e2203d73d9309fd3670690fe0e0376f58c60f800
trholdridge <trholdridge@gmail.com>
2024-02-08 18:09:49 -0500

    Collect returns from top level

--- example.cpp
+++ example.cpp
@@ -1,6 +1,11 @@
 #include "racket.h"
 
 int main() {
-​(define ⁣ real_main ⁣ ​(λ ⁣ (a ⁣ b ⁣ c) ⁣ (a + b)))
-‌(real_main ⁣ 3 ⁣ 4 ⁣ 5)
+    ​(define ⁣ real_main ⁣ ​(λ ⁣ (a ⁣ b ⁣ c) ⁣ (a + b)))
+    ​(define ⁣ make_thing ⁣ ​(λ ⁣ (a ⁣ b ⁣ c) ⁣ a))
+    ​(define ⁣ make_thing2 ⁣ ​(λ ⁣ (a ⁣ b ⁣ c) ⁣ ​(make_thing ⁣ c ⁣ b ⁣ a)))
+    ‌(real_main ⁣ 3 ⁣ 4 ⁣ 5)
+    ‌(real_main ⁣ 3 ⁣ 8 ⁣ 5)
+    ‌(make_thing ⁣ "test" ⁣ 8 ⁣ 5)
+    ‌(make_thing2 ⁣ 1 ⁣ 8 ⁣ 5)
 }
--- racket.h
+++ racket.h
@@ -1,6 +1,12 @@
 #ifndef RACKET_H_
 #define RACKET_H_
 
+#include <iostream>
+#include <concepts>
+#include <cstdint>
+#include <cxxabi.h>
+#include <vector>
+
 #define PARENS ()
 
 #define EXPAND(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
@@ -14,14 +20,40 @@
   __VA_OPT__(, FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
 #define FOR_EACH_AGAIN() FOR_EACH_HELPER
 
+template <typename T> 
+concept Streamable = requires (T x) { std::cout << x; };
+
+template<Streamable T>
+void general_print(T arg) {
+    std::cout << arg << std::endl;
+}
+
+template<typename T>
+typename std::enable_if<!Streamable<T>, void>::type general_print(T& arg) {
+    char *realname;
+    const std::type_info &ti = typeid(arg);
+    realname = abi::__cxa_demangle(ti.name(), NULL, NULL, NULL);
+    std::cout << realname <<  " at 0x" << (uint64_t)&arg << std::endl;
+    std::free(realname);
+}
+
+template<typename T>
+typename std::enable_if<!Streamable<T>, void>::type general_print(T&& arg) {
+    char *realname;
+    const std::type_info &ti = typeid(arg);
+    realname = abi::__cxa_demangle(ti.name(), NULL, NULL, NULL);
+    std::cout << realname <<  " at 0x" << (uint64_t)&arg << std::endl;
+    std::free(realname);
+}
+
 #define ⁣ ,
 #define ​(b) fun(b);
-#define ‌(b) return fun(b);
+#define ‌(b) general_print(fun(b));
 #define MAKE_AUTO(arg) auto arg
 #define fun(a, ...) a(__VA_ARGS__)
 #define define(x, value) auto x = value;
 #define GET_ARGS(...) __VA_ARGS__
-#define λ(args, body) [](FOR_EACH(MAKE_AUTO, GET_ARGS args)) {return body;};
-#define lambda(args, body) [](FOR_EACH(MAKE_AUTO, GET_ARGS args)) {return body;};
+#define lambda(args, body) [&](FOR_EACH(MAKE_AUTO, GET_ARGS args)) {return body;};
+#define λ lambda
 
 #endif
